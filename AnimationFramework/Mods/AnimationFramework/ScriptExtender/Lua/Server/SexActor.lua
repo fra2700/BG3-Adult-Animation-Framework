2daa1e9a3888211f1ecdeb022749507f7c98534c
LuneMods <155053912+LuneMods@users.noreply.github.com>
2024-02-12 21:04:03 +1000

    v1.1 release
    Removed 2x sounds as they don't really fit very well on a lot of actors

--- AnimationFramework/Mods/AnimationFramework/ScriptExtender/Lua/Server/SexActor.lua
+++ AnimationFramework/Mods/AnimationFramework/ScriptExtender/Lua/Server/SexActor.lua
@@ -13,8 +13,6 @@ local function BlockActorMovement(actor)
 end
 
 local ORGASM_SOUNDS = {
-    "Player_Races_Voice_Combat_Weak",
-    "Player_Races_Voice_Combat_Weak_Cinematics",
     "Player_Races_Voice_Combat_Recover",
     "Player_Races_Voice_Combat_Recover_Chance",
     "Player_Races_Voice_Combat_Recover_Cinematics",
