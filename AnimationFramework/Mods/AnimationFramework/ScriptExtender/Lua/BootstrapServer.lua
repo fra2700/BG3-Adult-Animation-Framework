5c418007321c670af5574ad33d827d18897ab835
Satans-Underpants <128394890+Satans-Underpants@users.noreply.github.com>
2024-02-25 15:53:11 +0100

    Added Hot Testing support

--- AnimationFramework/Mods/AnimationFramework/ScriptExtender/Lua/BootstrapServer.lua
+++ AnimationFramework/Mods/AnimationFramework/ScriptExtender/Lua/BootstrapServer.lua
@@ -1,3 +1,11 @@
+-- Hotloading
+local function on_reset_completed()
+    Ext.Stats.LoadStatsFile("Public/AnimationFramework/Stats/Generated/Data/AnimationFramework.txt",1)
+    _P('Reloading stats!')
+end
+
+Ext.Events.ResetCompleted:Subscribe(on_reset_completed)
+
 Ext.Require("Shared/AnimationPack.lua")
 Ext.Require("Server/AnimationFramework.lua")
 Ext.Require("Server/SexActor.lua")
